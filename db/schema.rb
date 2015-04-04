b341a5beba616d8fd3c1e56ba8907453fcf1a45d
Min Khati <minkhati@cdcsit.edu.np>
2015-04-04 17:46:43 +0000

    create chefs and recipes tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150404171552) do
+ActiveRecord::Schema.define(version: 20150404174005) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -21,10 +21,25 @@ ActiveRecord::Schema.define(version: 20150404171552) do
     t.integer  "user_id"
   end
 
+  create_table "chefs", force: true do |t|
+    t.string   "chefname"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "comments", force: true do |t|
     t.string   "commenter"
     t.text     "body"
     t.integer  "article_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "recipes", force: true do |t|
+    t.string   "name"
+    t.text     "summary"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
