83e3c4de405b9bc0a710fabfdedc57b848d46a5c
Min Khati <minkhati@cdcsit.edu.np>
2015-04-06 02:32:28 +0000

    add pagination

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150405203912) do
+ActiveRecord::Schema.define(version: 20150406003617) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 20150405203912) do
     t.string   "commenter"
     t.text     "body"
     t.integer  "article_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "likes", force: true do |t|
+    t.boolean  "like"
+    t.integer  "chef_id"
+    t.integer  "recipe_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
