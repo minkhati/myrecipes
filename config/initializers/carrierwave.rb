a4085e199e7b5df34bb18232be1c8b139eb2cf56
Min Khati <minkhati@cdcsit.edu.np>
2015-04-10 02:28:33 +0000

    some error remove

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,11 +5,11 @@ if Rails.env.production?
     
     config.fog_credentials = {
       :provider               => 'AWS',                         # required
-      :aws_access_key_id      => ENV['S3_ACCESS_KEY'],          # required
-      :aws_secret_access_key  => ENV['S3_SECRET_KEY'],          # required
+      :aws_access_key_id      => ENV['s3_ACCESS_KEY'],          # required
+      :aws_secret_access_key  => ENV['s3_SECRET_KEY'],          # required
       :region                 => 'us-east-1'                    # optional, defaults to 'us-east-1'
     }
-    config.fog_directory  = ENV['S3_BUCKET']                    # required
+    config.fog_directory  = ENV['s3_BUCKET']                    # required
     config.fog_public     = false                                   # optional, defaults to true
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
   end
