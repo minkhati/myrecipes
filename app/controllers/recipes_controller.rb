988cccc414d5f4e650f4661723a33b6d22d5ef03
Min Khati <minkhati@cdcsit.edu.np>
2015-04-12 00:44:46 +0000

    create styles and ingredients display and user interface

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,9 @@
 class RecipesController < ApplicationController
   
   before_action :set_recipe, only: [:edit, :update, :show, :like]
-  before_action :require_user, except: [:show, :index]
+  before_action :require_user, except: [:show, :index, :like]
+  before_action :require_user_like, only: [:like]
+  
   before_action :require_same_user, only: [:edit, :update] 
   
   
@@ -56,7 +58,7 @@ class RecipesController < ApplicationController
   private
   
     def recipe_params
-      params.require(:recipe).permit(:name, :summary, :description, :picture)
+      params.require(:recipe).permit(:name, :summary, :description, :picture, style_ids: [], ingredient_ids: [])
     end
     
     def set_recipe
@@ -67,6 +69,13 @@ class RecipesController < ApplicationController
       if current_user != @recipe.chef
         flash[:danger] = "You can only edit your own recipes"
         redirect_to recipes_path
+      end
+    end
+    
+    def require_user_like
+      if !logged_in?
+        flash[:danger] = "You must be logged in to perform that operation"
+        redirect_to :back
       end
     end
 end
